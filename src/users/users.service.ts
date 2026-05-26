d7224d916f9c8acf950f7a8877b0de2532dbc6c3
AElisee <2kange.eli@gmail.com>
2026-05-26 00:12:43 +0000

    hashing et ajout dans la bd

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
   findAll() {
-    return ['user1', 'user2', 'user3'];
+    return this.userRepository.find();
   }
 
-  create(createUserDto: CreateUserDto) {
-    return {
-      message: 'utilsateur créé',
-      data: createUserDto,
-    };
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    const newUser = this.userRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    return await this.userRepository.save(newUser);
   }
 }
